46563d88fa1a629c34ae53a84784e1b42aa783cb
Alex-WRX <82454730+Alex-WRX@users.noreply.github.com>
2021-04-19 20:11:39 +0300

    Revert "Lesson 1"

--- Task #6.py
+++
@@ -1,7 +0,0 @@
-firstday = 3
-actualday = 5
-day = 1
-while actualday - firstday > 0:
-    firstday = firstday + (firstday * 0.1)
-    day +=1
-print("За" , day,  "день спорстмен пробежал более" , actualday , "км")
